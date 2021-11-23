50862ba82042e7b747685064b2a90d6589120374
pomirko <p.pomirko@gmail.com>
2021-11-23 10:01:44 +0300

    About style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
-import About from "./pages/AboutPage/About";
+import About from "./Pages/AboutPage/About";
+import './App.scss';
 function App() {
   return <div> 
             <About />
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 import './Footer.scss';
 export default function Footer() {
     return (
-        <footer className='footer_main general_subtext'>
-            <section className='footer_simple-art__text'>Ⓒ Simple Art, 2021<br/>
+        <footer className='footer_main'>
+            <section className='footer_simple-art__text general_subtext'>Ⓒ Simple Art, 2021<br/>
             All rights reserved</section>   
             <section className='footer_nav'>
-                <a href='#' className='footer_link'>Terms</a>
-                <a href='#' className='footer_link'>Privacy policy</a>
-                <a href='#' className='footer_link'>Help&Support</a>
+                <a href='#' className='footer_link general_subtext'>Terms</a>
+                <a href='#' className='footer_link general_subtext'>Privacy policy</a>
+                <a href='#' className='footer_link general_subtext'>Help&Support</a>
             </section>
         </footer>
     );
