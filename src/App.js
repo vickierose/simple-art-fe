be0aa2060f72eed571161e19af239eb9bf4ebfa7
pomirko <p.pomirko@gmail.com>
2021-11-24 10:00:47 +0300

    Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Footer from "./Footer/Footer";
 import ModalSubscribe from "./modalSubscribe/modalSubscribe";
+import {useState} from 'react';
 
 function App() {
-  return <div>
-    <ModalSubscribe />
+  const [modalActive, setModalActive] = useState(false);
+
+  return (
+    <div>
+    
+     
+    <button className="primary-button" onClick={() => setModalActive(true)}>Subscribe</button>
+    <ModalSubscribe visible={modalActive} setVisible={setModalActive}/> 
     <Footer />
     </div>
+    ) 
 }
 
 export default App;
--- src/modalSubscribe/modalSubscribe.js
+++ src/modalSubscribe/modalSubscribe.js
@@ -2,36 +2,32 @@ import React from "react";
 import './modalSubscribe.scss';
 import Close_button from '../assets/img/close_button.png';
 import Modal_img from '../assets/img/modal-img.png';
+import {useState} from 'react';
 
 
-function ModalSubscribe (props) {
-    function hideModal(){
-        document.querySelector('.modal_main').setAttribute('style', 'display: none')
-    }
-    function openModal(){
-        document.querySelector('.modal_main').setAttribute('style', 'display: flex')
-    }
-    return (
+const ModalSubscribe = ({visible, setVisible}) => {
+     if(visible) {return ( 
         <div>
-            <button onClick={openModal}>Open Modal</button>
-        <section className='modal_main'>
-            <section className='modal_body'>
-                <section className='modal__close' onClick={hideModal}><img src={Close_button} /></section>
-                <section className='modal__input-field'>
-                    <section className="modal_body_text">
-                        <h2 className='header_h2_regular'>Give us your email to stay tuned!</h2>
-                        <form className="modal_form">
-                            <input type='email' className="modal_input" placeholder="E-mail" />
-                            <button className="primary-button">Subscribe</button>
-                        </form>
+                <section className='modal_main'>
+                    <section className='modal_body'>
+                        <section className='modal__close'><img src={Close_button} onClick={() => setVisible(false)}/></section>
+                        <section className='modal__input-field'>
+                            <section className="modal_body_text">
+                                <h2 className='header_h2_regular'>Give us your email to stay tuned!</h2>
+                                <form className="modal_form">
+                                    <input type='email' className="modal_input" placeholder="E-mail" />
+                                    <button className="primary-button" onClick={() => setVisible(false)}>Subscribe</button>
+                                </form>
+                            </section>
+                            <section className='modal__text'><p>You always can undo that in any of your received emails </p></section>
+                        </section>
                     </section>
-                    <section className='modal__text'><p>You always can undo that in any of your received emails </p></section>
+                    <section className='modal__img'><img src={Modal_img} alt='modal img' /></section>
                 </section>
-            </section>
-            <section className='modal__img'><img src={Modal_img} alt='modal img' /></section>
-        </section>
         </div>
-    )
+    )} else {return <div/>
+    }
+
 }
 
 export default ModalSubscribe;
